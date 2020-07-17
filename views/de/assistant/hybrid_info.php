1a9f4c5c3ce05041beb626aae85ba743d69c68db
Annelene Sudau <sudau@elan-ev.de>
2020-07-17 15:52:49 +0200

    textupdate Herausforderungen Hybride Lehre

--- views/de/assistant/hybrid_info.php
+++ views/de/assistant/hybrid_info.php
@@ -1,30 +1,29 @@
 <? if ($view === 'different_groups'): ?>
     <h2>Zwei unterschiedliche Zuhörendengruppen adäquat bespielen</h2>
     <ul>
-        <li>Kein primärer Aufmerksamkeitsfokus für digitale TN (langweilt Präsenz-TN)</li>
-        <li>Primärer Aufmerksamkeitsfokus auf Präsenz-TN ggf. sinnvoll, wenn aktive Beiträge von Präsenz-TN Unterricht beleben</li>
-        <li>Ansonsten grds. beide Gruppen gleichmäßig adressieren</li>
-        <li>Insbesondere sowohl Studierende in Präsenz als auch die Kamera ausgewogen anblicken</li>
-        <li>Soll Chat als Kommunikationskanal zu Digitalen TN dienen ist es zur Entlastung von Lehrenden sinnvoll, dass Präsenz-TN die Moderation des Kanals übernehmen</li>
+        <li>Primären Aufmerksamkeitsfokus nicht auf digitale TN legen (langweilt Präsenz-TN)</li>
+        <li>Primären Aufmerksamkeitsfokus ggf. auf Präsenz-TN legen, wenn aktive Beiträge von Präsenz-TN den Unterricht beleben, sonst grundsätzlich beide Gruppen gleichmäßig adressieren</li>
+        <li>Sowohl Präsenz-TN als auch digitale TN per Kamera ausgewogen anblicken</li>
+        <li>Soll der Chat als Kommunikationskanal mit den digitalen TN dienen, ist es zur Entlastung der Doz</li>
     </ul>
 
 
 <? elseif ($view === 'attention'): ?>
     <h2>Reduzierte Aufnahmefähigkeit im digitalen Kanal beachten</h2>
     <ul>
-        <li>Reine Vortragsszenarien zeitlich begrenzen (mehrere 10-minütige Inputs statt 90 Minuten Vorlesung)</li>
+        <li>Reine Vortragsszenarien zeitlich begrenzen (mehrere 10-minütige Beiträge, statt 90 Minuten Vorlesung)</li>
         <li>Wissensvermittlungskomponenten möglichst außerhalb der kostbaren Kontaktzeit platzieren</li>
         <li>Digitale TN direkt ansprechen (wenn interaktiv möglich)</li>
-        <li>Medienwechsel noch hilfreicher als in Präsenz</li>
-        <li>Präsenz- und Digital-TN verbindende kollaborative Werkzeuge zur Aktivierung einsetzen, z.B. Kleingruppenarbeit mit Etherpad</li>
-        <li>Ggf. TN in Gruppen aufteilen und Kontaktzeiten wechselweise mit den Gruppen verbringen, um Belastungen durch Gruppengröße zu reduzieren bzw. lernhindernde Distanzen zw. Studierenden und Lehrenden zu reduzieren und die Lehre ggf. bedarfsgemäß zu individualisieren.</li>
+        <li>Medienwechsel sind in der hybrigen Lehre noch hilfreicher als in reinen Präsenzszenarien</li>
+        <li>Digitale und Präsenz-TN durch den Einsatz kollaborativer Werkzeuge zur Aktivierung verbinden, z. B. Kleingruppenarbeit mit dem Etherpad</li>
+        <li>Ggf. TN in Gruppen aufteilen und Kontaktzeiten wechselweise mit den Gruppen verbringen, um die Belastung durch die Gruppengröße zu reduzieren bzw. lernhindernde Distanzen zwischen Studierenden und Lehrenden zu reduzieren und die Lehre ggf. bedarfsgemäß zu individualisieren</li>
     </ul>
 
 <? elseif ($view === 'burden'): ?>
     <h2>Präsenz- und Online-Teilnehmende gleichmäßig belasten</h2>
     <ul>
-        <li>Z.B.: Präsenzteilnehmer leisten Diskussionsbeiträge, Digitale TN bekommen zusätzliche Aufgaben (Protokollanteile, Rechercheaufgaben, Performance Spielräume -Aussenwette-)</li>
-        <li>Z.B.: Bei Kontrollfragen durch Lehrende gibt es gleichwertige Fragenblöcke speziell für Präsent-TN und speziell für digitale Kursbeteiligte</li>
+        <li>Z. B.: Präsenz-TN leisten Diskussionsbeiträge, digitale TN erhalten zusätzliche Aufgaben (Protokollanteile, Rechercheaufgaben, Performance-Spielräume)</li>
+        <li>Z. B.: Bei Kontrollfragen durch Dozent*innen jeweils gleichwertige Fragenblöcke speziell für Präsenz-TN und speziell fü digitale TN nutzen</li>
     </ul>
 <? endif ?>
 
