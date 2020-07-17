a5ef5a428d8b9770856662277b71553e93a11051
Annelene Sudau <sudau@elan-ev.de>
2020-07-17 12:46:00 +0200

    spelling and content

--- views/de/assistant/sem_format_info.php
+++ views/de/assistant/sem_format_info.php
@@ -3,8 +3,8 @@
         Online
     </h2>
     <ul>
-        <li>Lehre ausschließlich online</li>
-        <li>Gleiche Bedingungen und Möglichkeiten wie im Sommersemester 2020</li>
+        <li>Lehre erfolgt ausschließlich online</li>
+        <li>gleiche Bedingungen und Möglichkeiten wie im Sommersemester 2020</li>
         <li>Aber: Räume mit Aufzeichnungstechnik stehen, anders als im Online-Sommersemester, für dieses Format nun
         nicht mehr zur Verfügung</li>
     </ul>
@@ -14,24 +14,27 @@
         Hybrid
     </h2>
     
-    <p>Das Hybride Format ermöglicht Ihnen in diesem Semester wieder den direkten Kontakt zu Ihren Studierenden.
+    <p>Das hybride Format ermöglicht Ihnen in diesem Semester wieder den direkten Kontakt zu Ihren Studierenden.
         Aufgrund der Hygiene- und Abstandsvorgaben kann jedoch immer nur ein Teil der Studierenden anwesend
-        sein. Sobald Ihnen Räume zugewiesen wurden wissen Sie, wieviele Personen pro Termin anwesend sein können.
-        Die Zuteilung der Plätze auf Studierende kann wird durch Stud.IP unterstützt. Detaillierte Informationen
+        sein. Sobald Ihnen Räume zugewiesen wurden, wissen Sie, wie viele Studierende pro Termin anwesend sein können.
+        Die Zuteilung der Plätze auf Studierende wird durch Stud.IP unterstützt. Detaillierte Informationen
         hierzu folgen noch.
     </p>
     <p>
-        Wenn Sie sich für das Hybride Format entscheiden, sollten Sie einige Dinge beachten:
+        Wenn Sie sich für das hybride Format entscheiden, sollten Sie einige Punkte beachten:
     </p>
-    <ul>
-        <li>die technischen Anforderungen können je nach gewählter Umsetzungsvariante ggf. höher als bei reiner Online Lehre</li>
-        <li>benötigte Technik muss ggf. selbst angeschafft werden</li>
-        <li>immer nur ein Teil der Studierenden vor Ort, für nicht anwesende Studierende müssen Alternativen geschaffen werden</li>
-    </ul>
     <p>
-    Im Abschnitt <b>Hybrid-Lehre: Einstieg</b> finden Sie mögliche Umsetzungsvarianten für eine Hybride
-    Durchführung, sowie weiterführende Hinweise und Tipps.
-    Für die Auswahl der Personen die in Präsenz an einem termin teilnehmen dürfen sowie
+    <ul>
+        <li>nur ein Teil der Studierenden darf jeweils vor Ort sein, für nicht vor Ort anwesende Studierende müssen Alternativen geschaffen werden</li>
+        <li>beachten Sie, dass die Ihnen zur Verfügung gestellten Räume möglicherweise nicht mit Aufzeichnungstechnik ausgestattet sind</li>
+        <li>benötigte Technik muss ggf. selbst angeschafft werden</li>
+        <li>die technischen Anforderungen können je nach gewählter Umsetzungsvariante ggf. höher sein, als bei reiner Online-Lehre</li>
+    </ul>
+    </p>
+    <p>
+    Im Abschnitt <b>Hybrid-Lehre: Einstieg</b> finden Sie mögliche Umsetzungsvarianten für eine hybride
+    Durchführung sowie weiterführende Hinweise und Tipps.
+    Für die Auswahl der Studierenden, die in Präsenz an einem Termin teilnehmen dürfen, sowie für
     die Verteilung der Sitzplätze wird es unterstützende technische Möglichkeiten in Stud.IP geben.
     </p>
 
@@ -40,6 +43,6 @@
     </h2>
     <ul>
         <li>Präsenzlehre in den Räumen der Universität für alle Teilnehmenden</li>
-        <li>Nur unter bestimmten Bedingungen erlaubt</li>
-        <li>Muss explizit von StudiendekanInnen genehmigt werden</li>
+        <li>nur unter bestimmten Bedingungen erlaubt</li>
+        <li>muss explizit von den Studiendekaninnen/Studiendekanen genehmigt werden</li>
     </ul>
